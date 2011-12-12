0fa53969f8d867119f96cf0e16ca39d298557ad0
zhanglei <zhanglei_job_email@163.com>
2011-12-12 17:40:22 +0800

    ''

--- application/init.php
+++ application/init.php
@@ -3,7 +3,14 @@
  * 框架初始化文件
  * @author qingmu
  * @version
- * Created at:  2011-12-09 
+ * Created at:  2011-12-09
  */
+require_once 'cfg.php';
+require_once P_ROOT . '/../config/config.ini.php';
+require_once 'core.php';
 
+/// 初始化全局数据
+$GLOBALS[V_GLOBAL_NAME] = array();
+$GLOBALS[V_GLOBAL_NAME]['TPL'] = array();
+$GLOBALS[V_GLOBAL_NAME]['LANG'] = array();
 ?>
--- index.php
+++ index.php
@@ -6,9 +6,19 @@
  * Created at:  2011-12-09
  */
 //phpinfo();exit;
+// 入口名称
+define('ENTRY_SCRIPT_NAME', 'index');
+// 当前入口的默认模块路由
+define('R_DEF_MOD', "index");
+// 强制的路由模式　如果你尝试使用 rewrite功能失败，可以通过此选项快速恢复网站正常
+//define('R_FORCE_MODE', 0);
 
 /// 初始化框架
 require_once 'application/init.php';
+
+if (App::F('is_robot')) {
+	App::deny();
+}
 
 // 初始化应用程序
 App::init();
