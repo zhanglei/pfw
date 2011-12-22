560fd80b63738f51f1f9479109b301e231c58744
unknown <Administrator@WIN-OEOCS17ONEO.(none)>
2011-12-22 11:32:55 +0800

    ''

--- application/class/fastdfs.class.php
+++ application/class/fastdfs.class.php
@@ -1,3 +1,18 @@
 <?php
-    phpinfo();
+/** 
+ * 分布式文件系统 fastdfs类
+ * 
+ * @author qingmu
+ * @version 1.0
+ * Created at:  2011-12-09 
+ */    
+class Fastdfs {
+	
+	/**
+	 * 构造函数
+	 */
+	public function __construct() {
+
+	}
+}     
 ?>
--- application/class/gearman.class.php
+++ application/class/gearman.class.php
@@ -1,3 +1,19 @@
 <?php
-    phpinfo();
+/** 
+ * 分布式调度gearman类
+ * 
+ * @author qingmu
+ * @version 1.0
+ * Created at:  2011-12-09 
+ */
+class Gearman {
+	
+	/**
+	 * 构造函数
+	 */
+	public function __construct() {
+
+	}
+	
+}      
 ?>
