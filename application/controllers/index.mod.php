f9d833882a28f08d7abfddd1d96538f0d6643530
unknown <Administrator@WIN-OEOCS17ONEO.(none)>
2011-12-22 11:59:01 +0800

    ''

--- application/controllers/index.mod.php
+++ application/controllers/index.mod.php
@@ -1,3 +1,18 @@
 <?php
-    phpinfo();
+/**
+ * 首页控制器入口
+ * @author qingmu
+ * @version
+ * Created at:  2011-12-09
+ */
+class index_mod {
+	
+	/**
+	 * 构造函数
+	 */
+	public function __construct() {
+
+	}
+	
+} 
 ?>
