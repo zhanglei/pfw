e0205261841539ad00136e2e82adf23309305dfc
unknown <Administrator@WIN-OEOCS17ONEO.(none)>
2011-12-22 11:40:57 +0800

    ''

---
+++ application/class/ftp_io.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * FTP操作
+ *
+ * @author qingmu
+ * @version 1.0
+ * Created at:  2011-12-09
+ */
+class ftp_io {
+	function ftp_io() {
+
+	}
+
+	function adp_init($config = array()) {
+
+	}
+
+	function write($file, $contents, $is_append = false) {
+
+	}
+
+	function read($file) {
+
+	}
+
+	function delete($key) {
+
+	}
+
+	function mkdir($dirs) {
+
+	}
+
+	function rmdir($dir) {
+	}
+
+}
+?>
