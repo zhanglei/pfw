3ca288437e9abaaadeceeb06c0844adc15993820
unknown <Administrator@WIN-OEOCS17ONEO.(none)>
2011-12-22 12:04:34 +0800

    ''

--- application/controllers/admin/admin.mod.php
+++ application/controllers/admin/admin.mod.php
@@ -1,3 +1,25 @@
 <?php
-    phpinfo();
+/**
+ * 后台控制器入口
+ * @author qingmu
+ * @version
+ * Created at:  2011-12-09
+ */
+class admin_mod {
+	
+	/**
+	 * 构造函数
+	 */
+	public function __construct() {
+
+	}
+	
+	/**
+	 * 首页
+	 */
+	public function index() {
+		
+	}
+	
+} 
 ?>
