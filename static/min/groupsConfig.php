887f2bbbde9797bd1cc050e5d80bbf0befed9eab
unknown <Administrator@WIN-OEOCS17ONEO.(none)>
2012-01-15 15:30:43 +0800

    ''

--- static/min/groupsConfig.php
+++ static/min/groupsConfig.php
@@ -4,7 +4,7 @@
  * @package Minify
  */
 
-/** 
+/**
  * You may wish to use the Minify URI Builder app to suggest
  * changes. http://yourdomain/min/builder/
  *
@@ -12,8 +12,8 @@
  **/
 
 return array(
-    // '' => array('//js/file1.js', '//js/file2.js'),
-    // 'css' => array('//css/file1.css', '//css/file2.css'),	
-	'global_css' => array('//static/css/reset.css','//static/css/base.css','//static/css/global.css','//static/css/metacss.css'),
-	'global_js' => array(''),
-);
+		'global_css' => array('//static/css/reset.css', '//static/css/base.css', '//static/css/global.css', '//static/css/metacss.css'),
+		'global_js' => array('//static/js/jquery-1.7.1.js'),
+		'kindeditor' => array(''),
+		'calendar' => array('//static/widgets/calendar/WdatePicker.js') 
+	);
