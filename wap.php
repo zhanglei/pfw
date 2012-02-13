14c09502c12e1f4fbc1ae1e280b9bd5ef4e65dc8
unknown <Administrator@WIN-OEOCS17ONEO.(none)>
2012-02-13 12:40:32 +0800

    ''

--- wap.php
+++ wap.php
@@ -5,4 +5,24 @@
  * @version
  * Created at:  2011-12-09 
  */    
+//phpinfo();exit;
+// 入口名称
+define('ENTRY_SCRIPT_NAME', 'wap');
+// 当前入口的默认模块路由
+define('R_DEF_MOD', "wap/wap.index");
+// 强制的路由模式　如果你尝试使用 rewrite功能失败，可以通过此选项快速恢复网站正常
+//define('R_FORCE_MODE', 0);
+
+/// 初始化框架
+require_once 'application/init.php';
+
+if (App::F('is_robot')) {
+	App::deny();
+}
+
+// 初始化应用程序
+App::init();
+
+// 处理当前HTTP请求
+App::request(); 
 ?>
