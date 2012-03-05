a485a545de015938597d44d6af91aec92b4bc68d
unknown <zhanglei@.(none)>
2012-03-05 21:07:56 +0800

    ''

---
+++ application/class/dbshard.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 数据库分片接口
+ *
+ * @author qingmu
+ * @version 1.0
+ * Created at:  2011-12-09
+ */
+class DbShard {
+	
+	/**
+	 * contructor
+	 * @return void
+	 */
+	public function __construct() {
+
+	}
+}
+?>
