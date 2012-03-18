064b01cdcdad457bbb30d539d754506691b03886
unknown <zhanglei@.(none)>
2012-03-18 22:15:37 +0800

    ''

--- application/compat.php
+++ application/compat.php
@@ -167,4 +167,28 @@ function array_sort($array, $on, $order = SORT_ASC) {
 
 	return $new_array;
 }
+
+/**
+ * 分库算法 
+ */
+function calc_hash_db($u, $s = 4)
+{
+    $h = sprintf("%u", crc32($u));
+    $h1 = intval(fmod($h, $s));
+    return $h1;
+}
+
+/**
+ * 分表算法
+ */
+function calc_hash_tbl($u, $n = 16, $m = 16)
+{
+    $h = sprintf("%u", crc32($u));
+    $h1 = intval($h / $n);
+    $h2 = $h1 % $n;
+    $h3 = base_convert($h2, 10, $m);
+    $h4 = sprintf("%02s", $h3);
+    return $h4;
+}
+
 ?>
