b8db51abf3a136198162ca77c4a503ad9d77a5cb
unknown <zhanglei@.(none)>
2012-03-23 20:54:17 +0800

    ''

---
+++ application/controllers/user.mod.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 登陆控制器入口
+ * @author qingmu
+ * @version
+ * Created at:  2011-12-09
+ */
+class user_mod extends BaseMod{
+	
+	/**
+	 * 构造函数
+	 */
+	public function __construct() {
+
+	}
+	
+	/**
+	 * 
+	 */
+	public function login(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public function logout(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public function register(){
+		
+	}
+}
+
+?>
